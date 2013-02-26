73620158b60b8410807fb3bf2a89a33683a9821a
Martin Blom <martin@blom.org>
2013-02-26 22:20:51 +0100

    Added a highligt effect when an input field is filled in.

--- website/js/index.js
+++ website/js/index.js
@@ -12,6 +12,38 @@ $(document).ready(function() {
 
         $(el).removeClass('inactive').addClass('active');
     })
- 
+
     $(window).hashchange();
 });
+
+function demo_log(elem, type, value) {
+    var log = document.getElementById('demo-log');
+    var now = String(new Date()).substring(0, 24);
+
+    if (type != 'form') {
+        $(elem).addClass('highlight');
+        window.setTimeout(function() { $(elem).removeClass('highlight'); }, 1);
+    }
+
+    switch (type) {
+        case 'form':
+            log.innerHTML += now + ' Form "' + elem.getElementsByTagName('legend').item(0).innerHTML + '": ' + value + '<br/>';
+            break;
+
+        case 'username':
+            log.innerHTML += now + ' Username: ' + value + '<br/>';
+            break;
+
+        case 'password':
+            log.innerHTML += now + ' Password: ' + value + '<br/>';
+            break;
+
+        case 'new-password':
+            log.innerHTML += now + ' New password: ' + value + '<br/>';
+            break;
+
+        default:
+            log.innerHTML += now + ' Unknown type "' + type + '": ' + value + '<br/>';
+            break;
+    }
+}
